7b20bb0e4219277c9206125d184ffe29318fd676
takeuchi satoshi <takeuchi17401@gmail.com>
2014-05-30 14:30:11 +0900

    VLAN send OK

--- scapy/samplesend.py
+++ scapy/samplesend.py
@@ -15,15 +15,18 @@ if __name__ == '__main__':
     e = Ether()
     e.src = src
     e.dst = dst
-#    v = vlan.vlan(pcp=0, cfi=0, vid=100, ethertype='0x86dd')
-    u = IPv6()
-    u.src=srcip
-    u.dst=dstip
-    u.nh=data.IPPROTO_ICMPV6
+    d = Dot1Q()
+    d.prio = 0
+    d.id = 0
+    d.vlan = 100
+    i = IPv6()
+    i.src = srcip
+    i.dst = dstip
+    i.nh = data.IPPROTO_ICMPV6
     c = ICMPv6MLQuery()
     c.mladdr='ff38::1'
     
-    sendpkt=e/u/c
+    sendpkt=e/d/i/c
 #    sendpkt=Ether()/IPv6()/ICMPv6MLQuery_MLDv2()
     
     print ('sendpkt %s', sendpkt)
