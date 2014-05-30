133353d174890cc722e9c4cd152dd5d370809cd3
takeuchi satoshi <takeuchi17401@gmail.com>
2014-05-30 14:14:03 +0900

    ICMPv6MLQuery Send OK

--- scapy/samplesend.py
+++ scapy/samplesend.py
@@ -1,54 +1,30 @@
 # samplesend.py
-from scapy import *
 from scapy.all import *
-from scapy.config import conf
-from scapy.layers.l2 import *
-from scapy.layers.inet import *
-from scapy.fields import *
-from scapy.packet import *
-from scapy.volatile import *
-from scapy.sendrecv import sr,sr1,srp1
-from scapy.as_resolvers import AS_resolver_riswhois
-from scapy.supersocket import SuperSocket,L3RawSocket
-from scapy.arch import *
-from scapy.utils6 import *
 from scapy.layers.inet6 import *
-from scapy.packet import Packet
-from scapy.layers import inet6
 import data 
 
 if __name__ == '__main__':
-    src = "fe80::200:ff:fe00:1"
-    dst = "ff02::1:ff00:2"
+    src = "11:22:33:44:55:66"
+    dst = "66:55:44:33:22:11"
     srcip = "11::"
     dstip= "::11"
-#    fe80::d63d:7eff:fe4a:460c #self
-#    fe80::200:ff:fe00:1 #mininet h1
-#    fe80::200:ff:fe00:2 #mininet h2
-#    ff02::1:ff00:2 (ff02::1:ff00:2)
-#    ICMPv6MLReport_MLDv2()
 
-    #conf.verb = 0
     conf.iface = "eth0"
-    #myMacAddr = get_if_hwaddr(conf.iface)
-    myMacAddr = "fe80::d63d:7eff:fe4a:460c"
-    
+    myMacAddr = "00:00:00:00:00:02"
+
     e = Ether()
-    e.dst = dst
     e.src = src
+    e.dst = dst
 #    v = vlan.vlan(pcp=0, cfi=0, vid=100, ethertype='0x86dd')
     u = IPv6()
     u.src=srcip
     u.dst=dstip
-    u.payload = "Hello world"
+    u.nh=data.IPPROTO_ICMPV6
     c = ICMPv6MLQuery()
     c.mladdr='ff38::1'
-    sendpkt=e/u/c
     
-#    sendpkt=Ether()/IPv6()/ICMPv6MLQuery()
+    sendpkt=e/u/c
 #    sendpkt=Ether()/IPv6()/ICMPv6MLQuery_MLDv2()
     
     print ('sendpkt %s', sendpkt)
-#    send(sendpkt)
-#    sendp(x=sendpkt, iface=myMacAddr)
-    sendp(x=sendpkt)
+    sendp(sendpkt)
